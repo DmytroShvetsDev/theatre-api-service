64c117c9b7ceed8a4fac8efd26d938c679abd9e5
Dmytro Shvets <dirolius@gmail.com>
2023-07-20 17:10:07 +0300

    Add url, view & serializer for model Genre

--- theatre/urls.py
+++ theatre/urls.py
@@ -1,5 +1,13 @@
-from django.db import router
 from django.urls import path, include
+from rest_framework import routers
+
+from theatre.views import (
+    GenreViewSet,
+
+)
+
+router = routers.DefaultRouter()
+router.register("genres", GenreViewSet)
 
 urlpatterns = [path("", include(router.urls))]
 
